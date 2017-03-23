3f182e73e961194605489a36d141e05f2801068b
Philipp Minges <fjnalta@gmail.com>
2017-03-23 17:58:23 +0100

    basic character and network spell implementation
    -specific characterClasses inherit from virtual basic characterClass

---
+++ Assets/Scripts/Classes/MageCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MageCharacter : PlayerCharacter {
+
+	void Start() {
+		health = 200;
+		resource = 200;
+		secondResource = "Mana";
+	}
+
+	public override void Spell1() {
+		Debug.Log ("I am a Mage");
+
+	}
+
+	public override void Spell2() {
+		Debug.Log (curlevel);
+	}
+
+	public override void Spell3() {
+		Debug.Log (health);
+	}
+
+	public override void Spell4() {
+		Debug.Log (resource);
+	}
+
+	public override void Spell5() {
+		Debug.Log (secondResource);
+	}
+}
---
+++ Assets/Scripts/Classes/PlayerCharacter.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+/* Basic Character Class. Defining Stats and Spells here
+ * Fields have to be synced across the network */
+public abstract class PlayerCharacter : NetworkBehaviour {
+
+	private Material mat;
+
+	// Genaral
+	[SyncVar] private string playerName;
+
+	// Levels
+	private int maxlevel = 20;
+	[SyncVar] public int curlevel = 1;
+
+	//Basic Stats
+	[SyncVar] public int health;
+	[SyncVar] public int resource;
+	// Resource Name
+	[SyncVar] public string secondResource;
+
+
+	void Update () {
+		if (Input.GetButtonDown ("Spell1")) {
+			Spell1 ();
+			CmdCastSpell (Color.red);
+		}
+		if (Input.GetButtonDown ("Spell2")) {
+			Spell2 ();
+			CmdCastSpell (Color.blue);
+		}
+		if (Input.GetButtonDown ("Spell3")) {
+			Spell3 ();
+			CmdCastSpell (Color.green);
+		}
+		if (Input.GetButtonDown ("Spell4")) {
+			Spell4 ();
+		}
+		if (Input.GetButtonDown ("Spell5")) {
+			Spell5 ();
+		}
+	}
+
+	public virtual void Spell1() {
+		Debug.Log ("Spell1");
+	}
+
+	public virtual void Spell2() {
+		Debug.Log ("Spell2");
+	}
+
+	public virtual void Spell3() {
+		Debug.Log ("Spell3");
+	}
+
+	public virtual void Spell4() {
+		Debug.Log ("Spell4");
+	}
+
+	public virtual void Spell5() {
+		Debug.Log ("Spell5");
+	}
+
+	// Network related stuff
+	// ---------------------
+
+	// Process only on server
+	[Command]
+	public void CmdCastSpell(Color color) {
+		Debug.Log ("Server casted Spell and delegates to clients");
+		RpcProcessSpellCastEffects (color);
+	}
+
+	// Tell all Clients about the Changes
+	[ClientRpc]
+	public void RpcProcessSpellCastEffects(Color color) {
+		Debug.Log ("All clients process casted spell");
+		gameObject.GetComponent<Renderer> ().material.color = color;
+
+	}
+}
