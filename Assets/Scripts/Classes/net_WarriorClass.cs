1b6c3d0c3b987ee90387e9b4a98b58963276dff1
Philipp Minges <fjnalta@gmail.com>
2017-03-23 17:57:15 +0100

    remove unused classes from project

--- Assets/Scripts/Classes/net_WarriorClass.cs
+++ Assets/Scripts/Classes/net_WarriorClass.cs
@@ -1,62 +1,62 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.Networking;
-
-public class net_WarriorClass : net_CharacterClass {
-
-	public GameObject lvlupPrefab;
-
-	//Constructor
-	public net_WarriorClass(){
-
-		//Basic Stats
-		CharacterClassName = "Warrior";
-		Health = 100;
-		Rage = 100;
-		SecondResource = "Rage";
-
-
-		//set Base stats;
-		//Attackpower Formula: (Strength x 2) + (Char.Level x 3) - 20
-		AttackPower = (Strength * 2) + (CurLevel * 3) - 20;
-
-		//Stamina provides 1 health per stamina for the first 20 points of stamina,
-		//and 10 health per point of stamina thereafter.
-		Health += (Stamina * 8);
-
-	}
-
-	[Client]
-	public override void Spell1(GameObject target){
-		if (target != null) {
-			CmdApplyDamage (target, 10);
-//			Debug.Log ("Spell1 WARRIOR");
-		}
-	}
-
-	[Client]
-	public override void Spell2(){
-		Debug.Log (AttackPower);
-		Debug.Log (Stamina);
-	}
-
-	[Client]
-	public override void Spell3(){
-		LevelUp ();
-	}
-
-	[Client]
-	public override void LevelUp(){
-		Strength += 3;
-		Stamina += 2;
-
-		Health = 100 + (Strength * 8) + Stamina;
-		AttackPower = (Strength * 2) + (CurLevel * 3) - 20;
-		CurLevel += 1;
-
-		//Instantiate the lvlUpPrefab
-		GameObject lvlUp  = Instantiate (lvlupPrefab, transform.position, Quaternion.identity) as GameObject;
-		lvlUp.transform.parent = gameObject.transform;
-	}
-}
-
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.Networking;
+//
+//public class net_WarriorClass : net_CharacterClass {
+//
+//	public GameObject lvlupPrefab;
+//
+//	//Constructor
+//	public net_WarriorClass(){
+//
+//		//Basic Stats
+//		CharacterClassName = "Warrior";
+//		Health = 100;
+//		Rage = 100;
+//		SecondResource = "Rage";
+//
+//
+//		//set Base stats;
+//		//Attackpower Formula: (Strength x 2) + (Char.Level x 3) - 20
+//		AttackPower = (Strength * 2) + (CurLevel * 3) - 20;
+//
+//		//Stamina provides 1 health per stamina for the first 20 points of stamina,
+//		//and 10 health per point of stamina thereafter.
+//		Health += (Stamina * 8);
+//
+//	}
+//
+//	[Client]
+//	public override void Spell1(GameObject target){
+//		if (target != null) {
+//			CmdCastSpell (target, 10);
+////			Debug.Log ("Spell1 WARRIOR");
+//		}
+//	}
+//
+//	[Client]
+//	public override void Spell2(){
+//		Debug.Log (AttackPower);
+//		Debug.Log (Stamina);
+//	}
+//
+//	[Client]
+//	public override void Spell3(){
+//		LevelUp ();
+//	}
+//
+//	[Client]
+//	public override void LevelUp(){
+//		Strength += 3;
+//		Stamina += 2;
+//
+//		Health = 100 + (Strength * 8) + Stamina;
+//		AttackPower = (Strength * 2) + (CurLevel * 3) - 20;
+//		CurLevel += 1;
+//
+//		//Instantiate the lvlUpPrefab
+//		GameObject lvlUp  = Instantiate (lvlupPrefab, transform.position, Quaternion.identity) as GameObject;
+//		lvlUp.transform.parent = gameObject.transform;
+//	}
+//}
+//
--- Assets/Scripts/Player/net_PlayerUI.cs
+++ Assets/Scripts/Player/net_PlayerUI.cs
@@ -1,97 +1,96 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.Networking;
-using UnityEngine.UI;
-
-public class net_PlayerUI : NetworkBehaviour {
-
-	//GUI
-	private Image PlayerHP;
-	private Text PlayerHPText;
-	private Image PlayerMana;
-	private Image TargetHP;
-	private Text TargetNameText;
-	private Text TargetHPText;
-	private Image TargetHPBackground;
-	private Image TargetMana;
-
-	private net_PlayerInputManager inputManager;
-	private net_CharacterClass playerClass;
-
-	public override void OnStartLocalPlayer () {
-		//get GUI Components
-		PlayerHP = GameObject.Find("PlayerHP").GetComponent<Image> ();
-		PlayerHPText = GameObject.Find("PlayerHPText").GetComponent<Text> ();
-		PlayerMana = GameObject.Find ("PlayerMana").GetComponent<Image> ();
-		TargetHP = GameObject.Find("TargetHP").GetComponent<Image> ();
-		TargetHPBackground = GameObject.Find("TargetHPBackground").GetComponent<Image> ();
-		TargetNameText = GameObject.Find("TargetNameText").GetComponent<Text> ();
-		TargetHPText = GameObject.Find ("TargetHPText").GetComponent<Text> ();
-		TargetMana = GameObject.Find ("TargetMana").GetComponent<Image> ();
-
-		//get Target from InputManager
-		inputManager = GetComponent<net_PlayerInputManager> ();
-		//get characterspecific stuff from characterClass
-		playerClass = GetComponent<net_CharacterClass> ();
-	}
-
-	// Update is called once per frame
-	void Update () 
-	{
-		ShowTarget();
-	}
-
-	
-	void ShowTarget () {
-		if (isLocalPlayer) {
-			//Show TargetHPBar
-			if (inputManager.getTargetName () != "null") {
-				TargetHP.gameObject.SetActive (true);
-				TargetHPBackground.gameObject.SetActive(true);
-				if (inputManager.getTarget ().tag == "Minion") {
-					//if Minion get the MinionController to get the Life
-					TargetNameText.text = inputManager.getTargetName ();
-					TargetHP.color = new Color32(255,0,0,255);
-					TargetHP.fillAmount = inputManager.getTarget ().GetComponent<net_MinionController> ().Health / 100f;
-					TargetHPText.text = " " + inputManager.getTarget ().GetComponent<net_MinionController> ().Health + "/100";
-					TargetMana.fillAmount = 0f;
-				} else {
-					//always get the right character script from net_CharacterClass due to inheritance
-					//if its no minion it is a Player
-					if(inputManager.getTarget().GetComponent<net_CharacterClass>().CharacterClassName == "Warrior"){
-						//set TargetColor
-						TargetHP.color = new Color32(215,145,0,210);
-						//set RageColor
-						TargetMana.color = new Color32(255,0,0,255);
-					} else if(inputManager.getTarget().GetComponent<net_CharacterClass>().CharacterClassName == "Mage"){
-					
-						//TODO - Declare all class HP and Mana Color here!
-
-					} //and so on!!
-					TargetNameText.text = inputManager.getTarget().name;
-					TargetHP.fillAmount = inputManager.getTarget().GetComponent<net_CharacterClass>().Health / 100f;
-					TargetHPText.text = " " + inputManager.getTarget().GetComponent<net_CharacterClass>().Health + "/100";
-					TargetMana.fillAmount = inputManager.getTarget().GetComponent<net_CharacterClass>().Rage / 100f;
-				}
-
-			} else {
-				//if the TargetName is "null"
-				TargetHP.gameObject.SetActive (false);
-				TargetHPBackground.gameObject.SetActive(false);
-			}
-			//Show PlayerHPBar and get the color
-			if(GetComponent<net_CharacterClass>().CharacterClassName == "Warrior"){
-				PlayerHP.color = new Color32(215,145,0,210);
-				PlayerMana.color = new Color32(255,0,0,255);
-			} else if(GetComponent<net_CharacterClass>().CharacterClassName == "Mage"){
-			
-				// TODO - Declare all Player HP and Mana Colors
-
-			} //and so on!!
-
-			PlayerHP.fillAmount = playerClass.Health / 100f;
-			PlayerHPText.text = " " + playerClass.Health + "/100";
-			PlayerMana.fillAmount = playerClass.Rage / 100f;
-		}
-	}
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEngine.Networking;
+//using UnityEngine.UI;
+//
+//public class net_PlayerUI : NetworkBehaviour {
+//
+//	//GUI
+//	private Image PlayerHP;
+//	private Text PlayerHPText;
+//	private Image PlayerMana;
+//	private Image TargetHP;
+//	private Text TargetNameText;
+//	private Text TargetHPText;
+//	private Image TargetHPBackground;
+//	private Image TargetMana;
+//
+//	private net_CharacterClass playerClass;
+//
+//	public override void OnStartLocalPlayer () {
+//		//get GUI Components
+//		PlayerHP = GameObject.Find("PlayerHP").GetComponent<Image> ();
+//		PlayerHPText = GameObject.Find("PlayerHPText").GetComponent<Text> ();
+//		PlayerMana = GameObject.Find ("PlayerMana").GetComponent<Image> ();
+//		TargetHP = GameObject.Find("TargetHP").GetComponent<Image> ();
+//		TargetHPBackground = GameObject.Find("TargetHPBackground").GetComponent<Image> ();
+//		TargetNameText = GameObject.Find("TargetNameText").GetComponent<Text> ();
+//		TargetHPText = GameObject.Find ("TargetHPText").GetComponent<Text> ();
+//		TargetMana = GameObject.Find ("TargetMana").GetComponent<Image> ();
+//
+//		//get Target from InputManager
+//		inputManager = GetComponent<net_PlayerInputManager> ();
+//		//get characterspecific stuff from characterClass
+//		playerClass = GetComponent<net_CharacterClass> ();
+//	}
+//
+//	// Update is called once per frame
+//	void Update () 
+//	{
+//		ShowTarget();
+//	}
+//
+//	
+//	void ShowTarget () {
+//		if (isLocalPlayer) {
+//			//Show TargetHPBar
+//			if (inputManager.getTargetName () != "null") {
+//				TargetHP.gameObject.SetActive (true);
+//				TargetHPBackground.gameObject.SetActive(true);
+//				if (inputManager.getTarget ().tag == "Minion") {
+//					//if Minion get the MinionController to get the Life
+//					TargetNameText.text = inputManager.getTargetName ();
+//					TargetHP.color = new Color32(255,0,0,255);
+//					TargetHP.fillAmount = inputManager.getTarget ().GetComponent<net_MinionController> ().Health / 100f;
+//					TargetHPText.text = " " + inputManager.getTarget ().GetComponent<net_MinionController> ().Health + "/100";
+//					TargetMana.fillAmount = 0f;
+//				} else {
+//					//always get the right character script from net_CharacterClass due to inheritance
+//					//if its no minion it is a Player
+//					if(inputManager.getTarget().GetComponent<net_CharacterClass>().CharacterClassName == "Warrior"){
+//						//set TargetColor
+//						TargetHP.color = new Color32(215,145,0,210);
+//						//set RageColor
+//						TargetMana.color = new Color32(255,0,0,255);
+//					} else if(inputManager.getTarget().GetComponent<net_CharacterClass>().CharacterClassName == "Mage"){
+//					
+//						//TODO - Declare all class HP and Mana Color here!
+//
+//					} //and so on!!
+//					TargetNameText.text = inputManager.getTarget().name;
+//					TargetHP.fillAmount = inputManager.getTarget().GetComponent<net_CharacterClass>().Health / 100f;
+//					TargetHPText.text = " " + inputManager.getTarget().GetComponent<net_CharacterClass>().Health + "/100";
+//					TargetMana.fillAmount = inputManager.getTarget().GetComponent<net_CharacterClass>().Rage / 100f;
+//				}
+//
+//			} else {
+//				//if the TargetName is "null"
+//				TargetHP.gameObject.SetActive (false);
+//				TargetHPBackground.gameObject.SetActive(false);
+//			}
+//			//Show PlayerHPBar and get the color
+//			if(GetComponent<net_CharacterClass>().CharacterClassName == "Warrior"){
+//				PlayerHP.color = new Color32(215,145,0,210);
+//				PlayerMana.color = new Color32(255,0,0,255);
+//			} else if(GetComponent<net_CharacterClass>().CharacterClassName == "Mage"){
+//			
+//				// TODO - Declare all Player HP and Mana Colors
+//
+//			} //and so on!!
+//
+//			PlayerHP.fillAmount = playerClass.Health / 100f;
+//			PlayerHPText.text = " " + playerClass.Health + "/100";
+//			PlayerMana.fillAmount = playerClass.Rage / 100f;
+//		}
+//	}
+//}
