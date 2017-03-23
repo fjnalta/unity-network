d8cd2b6a4ccb68c4149ebc287ffde9d4f970e7f9
Philipp Minges <fjnalta@gmail.com>
2017-03-24 00:26:42 +0100

    import assets for weekend
    changes to player prefab
    changes to mouseover script
    import spells and characters

---
+++ Assets/Scripts/GameManager/CharacterMouseOver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CharacterMouseOver : NetworkBehaviour {
+
+	private Renderer ren;
+	private Shader originalShader;
+	private Shader outline;
+
+
+	void Awake() {
+		outline = Shader.Find ("Custom/Outline");
+		originalShader = Shader.Find("Legacy Shaders/Self-Illumin/Bumped Diffuse");
+	}
+
+	void Start() {
+		ren = GetComponentInChildren<SkinnedMeshRenderer> ();
+	}
+
+	void OnMouseOver() {
+		if (!isLocalPlayer) {
+			for(int i = 0; i < ren.materials.Length; i++) {
+				ren.materials[i].shader = outline;
+			}
+		}
+	}
+
+	void OnMouseExit() {
+		if (!isLocalPlayer) {
+			for (int i = 0; i < ren.materials.Length; i++) {
+				ren.materials[i].shader = originalShader;
+			}
+		}
+	}
+}
